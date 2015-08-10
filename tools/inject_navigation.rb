9a1c3082343f9d6b51379556636fe6ed39033df2
Marcus Rohrmoser <mrohrmoser@acm.org>
2015-08-10 22:38:56 +0200

    detab

--- tools/inject_navigation.rb
+++ tools/inject_navigation.rb
@@ -10,87 +10,87 @@ require File.join(File.dirname(__FILE__), 'xhtml.rb')
 # TODO visualise as site-map.
 class NavigationInjector < Navigation
 
-	def initialize src
-		super src
-	end
+  def initialize src
+    super src
+  end
 
-	# Iterate and delegate to inject_single
-	# [dsts] either a relative path to a single file or an array of such.
-	def inject dsts
-		dsts = [dsts] if !dsts.kind_of? Array
-		dsts.each do |dst|
-			begin
-				inject_single dst
-			rescue Exception => e
-				$stderr.puts "Warning: #{e}"
-			end
-		end
-	end
+  # Iterate and delegate to inject_single
+  # [dsts] either a relative path to a single file or an array of such.
+  def inject dsts
+    dsts = [dsts] if !dsts.kind_of? Array
+    dsts.each do |dst|
+      begin
+        inject_single dst
+      rescue Exception => e
+        $stderr.puts "Warning: #{e}"
+      end
+    end
+  end
 
 private
-	# Inject navigation into one html page.
-	# [dst] relative path of the page to inject into. 
-	# The path is interpreted both as the path to the physical
-	# file to inject into AND the relative URL of the page on the website.
-	def inject_single dst, log=$stderr
-		log.puts "Injecting #{dst}"
-		# eagerly load the page xhtml to inject into:
-		xhtml = loadXhtml dst
+  # Inject navigation into one html page.
+  # [dst] relative path of the page to inject into. 
+  # The path is interpreted both as the path to the physical
+  # file to inject into AND the relative URL of the page on the website.
+  def inject_single dst, log=$stderr
+    log.puts "Injecting #{dst}"
+    # eagerly load the page xhtml to inject into:
+    xhtml = loadXhtml dst
 
-		# prefix relative paths
-		# get the nesting level compared to the cwd
-		cwd = File.expand_path('.')
-		pwd = File.dirname(File.expand_path(dst))
-		nesting = pwd.count('/') - cwd.count('/')
-		prefix = '../' * nesting
+    # prefix relative paths
+    # get the nesting level compared to the cwd
+    cwd = File.expand_path('.')
+    pwd = File.dirname(File.expand_path(dst))
+    nesting = pwd.count('/') - cwd.count('/')
+    prefix = '../' * nesting
 
-		# clone the whole navigation as is:
-		x = @Xml.deep_clone
+    # clone the whole navigation as is:
+    x = @Xml.deep_clone
 
-		# find the desired page's navigation node:
-		node = x.get_elements("descendant-or-self::node[@href='#{dst}']")
-		raise "Page [#{dst}] not found in navigation structure." if node.length == 0
-		raise "Page [#{dst}] found multiple times in navigation structure." if node.length > 1
-		node = node[0]
+    # find the desired page's navigation node:
+    node = x.get_elements("descendant-or-self::node[@href='#{dst}']")
+    raise "Page [#{dst}] not found in navigation structure." if node.length == 0
+    raise "Page [#{dst}] found multiple times in navigation structure." if node.length > 1
+    node = node[0]
 
-		# mark all 'node' children of the node and all it's ancestors:
-		xpath = ['ancestor-or-self::/node']
-		# mark all 1st level child anchors of the node
-		xpath << 'node[starts-with(@href,\'#\')]'
-		# mark all 2nd level child anchors of the node
-		xpath << 'node[starts-with(@href,\'#\')]/node[starts-with(@href,\'#\')]'
-		node.each_element(xpath.join('|')) { |c| c.add_attribute 'keep', 'true' }
+    # mark all 'node' children of the node and all it's ancestors:
+    xpath = ['ancestor-or-self::/node']
+    # mark all 1st level child anchors of the node
+    xpath << 'node[starts-with(@href,\'#\')]'
+    # mark all 2nd level child anchors of the node
+    xpath << 'node[starts-with(@href,\'#\')]/node[starts-with(@href,\'#\')]'
+    node.each_element(xpath.join('|')) { |c| c.add_attribute 'keep', 'true' }
 
-		# remove all unmarked elements
-		x.each_element("descendant::node[not(@keep='true')]") {|e| e.remove}
+    # remove all unmarked elements
+    x.each_element("descendant::node[not(@keep='true')]") {|e| e.remove}
 
-		# create xhtml snippet to inject
-		div = <<END_OF_NAVIGATION_HEAD
+    # create xhtml snippet to inject
+    div = <<END_OF_NAVIGATION_HEAD
 <div id='navigation'>
 <!-- 
 this navigation node is auto-generated and injected
 by the script #{__FILE__}
 
-DO NOT EDIT IT MANUALLY!!!	
+DO NOT EDIT IT MANUALLY!!!  
 -->
 <p class="header">
-	<a href='#{prefix}index.html'><img src='#{prefix}images/Piccolo2D-Logo-small.png' alt='Piccolo2D Logo' /></a>
+  <a href='#{prefix}index.html'><img src='#{prefix}images/Piccolo2D-Logo-small.png' alt='Piccolo2D Logo' /></a>
 </p>
 END_OF_NAVIGATION_HEAD
-		Navigation.walk_tree_xml(x, div, "  ") do |_dst,child|
-			title = child.attribute('title').value
-			href = child.attribute('href').value
-			href = prefix + href if href[0,1] != '#' # don't prefix anchors
-			if child == node
-				_dst << "<a class='selected' href='#{href}'>#{title}</a>"
-			else
-				_dst << "<a href='#{href}'>#{title}</a>"
-			end
-		end
-		div << <<END_OF_NAVIGATION_FOOT
+    Navigation.walk_tree_xml(x, div, "  ") do |_dst,child|
+      title = child.attribute('title').value
+      href = child.attribute('href').value
+      href = prefix + href if href[0,1] != '#' # don't prefix anchors
+      if child == node
+        _dst << "<a class='selected' href='#{href}'>#{title}</a>"
+      else
+        _dst << "<a href='#{href}'>#{title}</a>"
+      end
+    end
+    div << <<END_OF_NAVIGATION_FOOT
 <!--
 <p>
-	<img src='#{prefix}images/giny-small.png' alt='a nice graph' />
+  <img src='#{prefix}images/giny-small.png' alt='a nice graph' />
 </p>
 -->
 <p class='bugreport'>
@@ -98,27 +98,27 @@ END_OF_NAVIGATION_HEAD
 </p>
 
 <p class="validators">
-	<a href='http://validator.w3.org/check/referer'><img src='http://www.w3.org/Icons/valid-xhtml10-blue'
-	alt='Valid XHTML 1.0!' style='border:0;width:88px;height:31px' /></a>
-	<a href='http://jigsaw.w3.org/css-validator/check/referer'><img src='http://www.w3.org/Icons/valid-css-blue'
-	alt='Valid CSS1!' style='border:0;width:88px;height:31px' /></a>
+  <a href='http://validator.w3.org/check/referer'><img src='http://www.w3.org/Icons/valid-xhtml10-blue'
+  alt='Valid XHTML 1.0!' style='border:0;width:88px;height:31px' /></a>
+  <a href='http://jigsaw.w3.org/css-validator/check/referer'><img src='http://www.w3.org/Icons/valid-css-blue'
+  alt='Valid CSS1!' style='border:0;width:88px;height:31px' /></a>
 </p>
 
 </div>
 END_OF_NAVIGATION_FOOT
-		# parse
-		div = REXML::Document.new div
+    # parse
+    div = REXML::Document.new div
 
-		# inject
-		n = xhtml.get_elements("descendant::*[@id='navigation']")
-		raise "Id 'navigation' not found in page [#{dst}]." if n.length == 0
-		raise "Id 'navigation' found multiple times in page [#{dst}]." if n.length > 1
-		n = n[0]
-		n.parent.replace_child n, div
+    # inject
+    n = xhtml.get_elements("descendant::*[@id='navigation']")
+    raise "Id 'navigation' not found in page [#{dst}]." if n.length == 0
+    raise "Id 'navigation' found multiple times in page [#{dst}]." if n.length > 1
+    n = n[0]
+    n.parent.replace_child n, div
 
-		# write back the injected file
-		xhtml.write File.open(dst, "w")
-	end
+    # write back the injected file
+    xhtml.write File.open(dst, "w")
+  end
 end
 
 ###########################################################
@@ -126,19 +126,19 @@ end
 ###########################################################
 
 if ARGV.length < 1 || ARGV[0] == '--help' || ARGV[0] == '-help' || ARGV[0] == '-?'
-	puts <<END_OF_HELP
+  puts <<END_OF_HELP
 Inject navigation from tools/navigation.txt into html pages.
 
 Usage:
-	1) cd to the root of the website
-	2) call this script with multiple html files as parameters
+  1) cd to the root of the website
+  2) call this script with multiple html files as parameters
 
 Example:
-	$ cd ~/work/piccolo2d/site.stage
-	$ #{__FILE__} index.html about.html
+  $ cd ~/work/piccolo2d/site.stage
+  $ #{__FILE__} index.html about.html
 
 END_OF_HELP
-	exit
+  exit
 end
 
 n = NavigationInjector.new "#{File.dirname(__FILE__)}/navigation.txt"
--- tools/xhtml.rb
+++ tools/xhtml.rb
@@ -6,256 +6,256 @@ require 'net/http'
 
 # Helper to read a textual navigation structure from a file.
 class Navigation
-	# Read the textual navigation structure and provide an
-	# iterator (level,url,title) of each entry.
-	# Comment lines starting with # are ignored.
-	# 
-	# Yields level, url, title
-	# [src] either a path to the navigation.txt file or an IO or something else supporting each_line.
-	def self.tokenize_navigation src
-		src = File.new src if src.kind_of? String
-		# Parse the navigation txt
-		comment = /^(#.*?)?\s*$/
-		pat = /^(\t*)(\S+)\s+(.+?)\s*$/
-		src.each_line do |line|
-			#puts line.strip
-			next if !comment.match(line).nil?
-			m = pat.match line
-			if m.nil?
-				$stderr.puts "Mismatching entry: [#{line}]"
-				next
-			end
-			yield m[1].length, m[2], m[3]
-		end
-	end
+  # Read the textual navigation structure and provide an
+  # iterator (level,url,title) of each entry.
+  # Comment lines starting with # are ignored.
+  # 
+  # Yields level, url, title
+  # [src] either a path to the navigation.txt file or an IO or something else supporting each_line.
+  def self.tokenize_navigation src
+    src = File.new src if src.kind_of? String
+    # Parse the navigation txt
+    comment = /^(#.*?)?\s*$/
+    pat = /^(\t*)(\S+)\s+(.+?)\s*$/
+    src.each_line do |line|
+      #puts line.strip
+      next if !comment.match(line).nil?
+      m = pat.match line
+      if m.nil?
+        $stderr.puts "Mismatching entry: [#{line}]"
+        next
+      end
+      yield m[1].length, m[2], m[3]
+    end
+  end
 
-	# Read the textual navigation structure and return a xml document
-	# for further processing.
-	# [src] input for tokenize_navigation(src)
-	def self.parse_navigation_xml src
-		xml = REXML::Document.new
-		current = xml
-		curr_lev = -1
-		self.tokenize_navigation(src) do |level,href,title|
-			# keep html entities in the title - don't escape the &:
-			elem = REXML::Document.new "<node title='#{title.strip.gsub ' ', '&nbsp;'}'/>"
-			elem = elem.root
-			elem.add_attribute 'href', href
-			elem.add_attribute 'level', level.to_s
-			if level == curr_lev
-				pa = current.parent
-			elsif level > curr_lev
-				pa = current
-				curr_lev += 1
-			else
-				pa = current.parent
-				while level < curr_lev
-					pa = pa.parent
-					curr_lev -= 1
-				end
-			end
-			pa.add(current=elem)
-		end
-		xml
-	end
+  # Read the textual navigation structure and return a xml document
+  # for further processing.
+  # [src] input for tokenize_navigation(src)
+  def self.parse_navigation_xml src
+    xml = REXML::Document.new
+    current = xml
+    curr_lev = -1
+    self.tokenize_navigation(src) do |level,href,title|
+      # keep html entities in the title - don't escape the &:
+      elem = REXML::Document.new "<node title='#{title.strip.gsub ' ', '&nbsp;'}'/>"
+      elem = elem.root
+      elem.add_attribute 'href', href
+      elem.add_attribute 'level', level.to_s
+      if level == curr_lev
+        pa = current.parent
+      elsif level > curr_lev
+        pa = current
+        curr_lev += 1
+      else
+        pa = current.parent
+        while level < curr_lev
+          pa = pa.parent
+          curr_lev -= 1
+        end
+      end
+      pa.add(current=elem)
+    end
+    xml
+  end
 
-	# Render the navigation structure (xml as from parse_navigation_xml)
-	# into nested <ul> lists.
-	#
-	# Titles only, no links.
-	#
-	# [parent] the xml node to print
-	# [dst] destination. Must support <<
-	# [level] indentation level
-	def self.print_tree_xml parent, dst=$stdout, level=0
-		self.walk_tree_xml(parent, dst, "  ", level) do |dst,child|
-			dst << child.attribute('title').value
-		end
-	end
+  # Render the navigation structure (xml as from parse_navigation_xml)
+  # into nested <ul> lists.
+  #
+  # Titles only, no links.
+  #
+  # [parent] the xml node to print
+  # [dst] destination. Must support <<
+  # [level] indentation level
+  def self.print_tree_xml parent, dst=$stdout, level=0
+    self.walk_tree_xml(parent, dst, "  ", level) do |dst,child|
+      dst << child.attribute('title').value
+    end
+  end
 
-	# Closure to render the navigation structure (xml as from parse_navigation_xml)
-	# into nested <ul> lists.
-	# 
-	# yields dst,child
-	#
-	# [parent] the xml node to print
-	# [dst] destination. Must support <<
-	# [indent] indentation atom
-	# [level] indentation level
-	def self.walk_tree_xml parent, dst, indent="\t", level=0, &block
-		# yield and recursion: 
-		# http://www.google.com/search?q=ruby+yield+recursive
-		# http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/76211
-		indent = indent * level
-		dst << "<ul>\n"
-		parent.each_child do |child|
-			dst << "#{indent}<li>"
-			block.call(dst,child)
-			walk_tree_xml(child, dst, indent, level+1, &block) if child.has_elements?
-			dst << "</li>\n"
-		end
-		dst << "#{indent}</ul>"
-	end
+  # Closure to render the navigation structure (xml as from parse_navigation_xml)
+  # into nested <ul> lists.
+  # 
+  # yields dst,child
+  #
+  # [parent] the xml node to print
+  # [dst] destination. Must support <<
+  # [indent] indentation atom
+  # [level] indentation level
+  def self.walk_tree_xml parent, dst, indent="\t", level=0, &block
+    # yield and recursion: 
+    # http://www.google.com/search?q=ruby+yield+recursive
+    # http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/76211
+    indent = indent * level
+    dst << "<ul>\n"
+    parent.each_child do |child|
+      dst << "#{indent}<li>"
+      block.call(dst,child)
+      walk_tree_xml(child, dst, indent, level+1, &block) if child.has_elements?
+      dst << "</li>\n"
+    end
+    dst << "#{indent}</ul>"
+  end
 
-	def initialize src
-		@Xml = Navigation.parse_navigation_xml(src)
-		# $stderr.puts @Xml
-	end
+  def initialize src
+    @Xml = Navigation.parse_navigation_xml(src)
+    # $stderr.puts @Xml
+  end
 
 protected
-	# delegates to Page.loadXml
-	def loadXhtml src
-		Page.loadXml src
-	end
+  # delegates to Page.loadXml
+  def loadXhtml src
+    Page.loadXml src
+  end
 
 end
 
 
 class Page
 
-	def self.makeAbsoluteUrl(href)
-		return nil if href.nil? || href.kind_of?(IO)
-		if href.kind_of? String
-			pat = /^([^:]+):\/\/([^\/:]*)(?::([0-9]+))?(.*)$/
-			url = pat.match href
-			return url if !url.nil?
-			url = pat.match "file://localhost#{File.expand_path(href)}"
-			raise "Pattern mismatch: file://localhost#{File.expand_path(href)}" if url.nil?
-			return url
-		end
-		raise "Unsupported input type [#{href.class}]"
-	end
+  def self.makeAbsoluteUrl(href)
+    return nil if href.nil? || href.kind_of?(IO)
+    if href.kind_of? String
+      pat = /^([^:]+):\/\/([^\/:]*)(?::([0-9]+))?(.*)$/
+      url = pat.match href
+      return url if !url.nil?
+      url = pat.match "file://localhost#{File.expand_path(href)}"
+      raise "Pattern mismatch: file://localhost#{File.expand_path(href)}" if url.nil?
+      return url
+    end
+    raise "Unsupported input type [#{href.class}]"
+  end
 
-	def self.loadXml(href)
-		return nil if href.nil?
-		return REXML::Document.new(href) if href.kind_of? IO
-		if href.kind_of? String
-			pat = /^([^:]+):\/\/([^\/:]*)(?::([0-9]+))?(.*)$/
-			url = pat.match href
-			if url.nil?
-				return REXML::Document.new(File.new(href, 'r'))
-			elsif url[1] == 'file'
-#				$stderr.puts "Huhu! #{url[1]} #{url[2]} #{url[3]} #{url[4]}"
-				return REXML::Document.new(File.new(url[4], 'r'))
-			elsif url[1] == 'http'
-				http = Net::HTTP.new(url[2], url[3].nil?? 80 : url[3].to_i)					
-				response = http.get(url[4], nil )
-				return REXML::Document.new(response.body)
-			else
-				raise "Unsupported protocol [#{m[1]}] in #{href}"
-			end
-		end
-		raise "Unsupported input type [#{href.class}]"
-	end
+  def self.loadXml(href)
+    return nil if href.nil?
+    return REXML::Document.new(href) if href.kind_of? IO
+    if href.kind_of? String
+      pat = /^([^:]+):\/\/([^\/:]*)(?::([0-9]+))?(.*)$/
+      url = pat.match href
+      if url.nil?
+        return REXML::Document.new(File.new(href, 'r'))
+      elsif url[1] == 'file'
+#       $stderr.puts "Huhu! #{url[1]} #{url[2]} #{url[3]} #{url[4]}"
+        return REXML::Document.new(File.new(url[4], 'r'))
+      elsif url[1] == 'http'
+        http = Net::HTTP.new(url[2], url[3].nil?? 80 : url[3].to_i)         
+        response = http.get(url[4], nil )
+        return REXML::Document.new(response.body)
+      else
+        raise "Unsupported protocol [#{m[1]}] in #{href}"
+      end
+    end
+    raise "Unsupported input type [#{href.class}]"
+  end
 
 public
-	# replace <a name="..."> with <a id="..."> and remove the <a> tag if it's only purpose is the anchor
-	def self.fix_anchors(xhtml,report=nil)
-		xhtml = xhtml.body if xhtml.kind_of? Page
-		# first build a list of all <a> tags
-		all_a = []
-		xhtml.elements.each("//a") do |a|
-			if a.children.length == 0
-				all_a << a
-				next
-			end
-			if a.children.length == 1 && a.children[0].kind_of?(REXML::Text) && a.parent.children.length == 1
-				all_a << a
-				next
-			end
-		end
-		all_a.each do |a|
-			a.add_attribute('id', a.attributes['name']) if !a.attributes['name'].nil? && a.attributes['id'] != a.attributes['name']
-			a.delete_attribute 'name'
-			p = a.parent
-			# parent has an id?
-			next if !p.attributes['id'].nil?
-			next if a.attributes.length > 1
-			p.add_attribute 'id', a.attributes['id']
-			a.remove
-			report.puts "#{p.to_s.gsub(/\s+/, ' ')}" if report
-		end
-		xhtml
-	end
+  # replace <a name="..."> with <a id="..."> and remove the <a> tag if it's only purpose is the anchor
+  def self.fix_anchors(xhtml,report=nil)
+    xhtml = xhtml.body if xhtml.kind_of? Page
+    # first build a list of all <a> tags
+    all_a = []
+    xhtml.elements.each("//a") do |a|
+      if a.children.length == 0
+        all_a << a
+        next
+      end
+      if a.children.length == 1 && a.children[0].kind_of?(REXML::Text) && a.parent.children.length == 1
+        all_a << a
+        next
+      end
+    end
+    all_a.each do |a|
+      a.add_attribute('id', a.attributes['name']) if !a.attributes['name'].nil? && a.attributes['id'] != a.attributes['name']
+      a.delete_attribute 'name'
+      p = a.parent
+      # parent has an id?
+      next if !p.attributes['id'].nil?
+      next if a.attributes.length > 1
+      p.add_attribute 'id', a.attributes['id']
+      a.remove
+      report.puts "#{p.to_s.gsub(/\s+/, ' ')}" if report
+    end
+    xhtml
+  end
 
-	def initialize(href)
-		#@Href = href
-		@Url = Page.makeAbsoluteUrl(href)
-		@Body = Page.fix_anchors(Page.loadXml(href))		
-		$stderr.puts "Not well formed: #{href}" if @Body.nil?
-		t = @Body.elements['html/head/title']
-		@Title = t.nil?? nil : t.text.strip
-	end
+  def initialize(href)
+    #@Href = href
+    @Url = Page.makeAbsoluteUrl(href)
+    @Body = Page.fix_anchors(Page.loadXml(href))    
+    $stderr.puts "Not well formed: #{href}" if @Body.nil?
+    t = @Body.elements['html/head/title']
+    @Title = t.nil?? nil : t.text.strip
+  end
 
-	def each_h
-	        @Body.elements.each("//*") do |e|
-			pat = /^h([1-6])$/
-			m = pat.match(e.name)
-			next if m.nil?
-			yield m[1].to_i, e.texts
-		end
-	end
+  def each_h
+          @Body.elements.each("//*") do |e|
+      pat = /^h([1-6])$/
+      m = pat.match(e.name)
+      next if m.nil?
+      yield m[1].to_i, e.texts
+    end
+  end
 
-	def each_a
-	        @Body.elements.each("//*") do |e|
-			yield e if e.name == 'a'
-		end
-	end
+  def each_a
+          @Body.elements.each("//*") do |e|
+      yield e if e.name == 'a'
+    end
+  end
 
-	def body
-		@Body
-	end
+  def body
+    @Body
+  end
 
-	def title
-		@Title
-	end
+  def title
+    @Title
+  end
 
-	def url
-		@Url
-	end
+  def url
+    @Url
+  end
 end
 
 class Site
-	def initialize(hrefs)
-		@Url2Page = {}
-		@Title2Url = {}
-		hrefs.each do |href|
-			p = Page.new(href)
-			@Url2Page[p.url] = p
-			@Title2Url[p.title] = p.url
-		end
-	end
+  def initialize(hrefs)
+    @Url2Page = {}
+    @Title2Url = {}
+    hrefs.each do |href|
+      p = Page.new(href)
+      @Url2Page[p.url] = p
+      @Title2Url[p.title] = p.url
+    end
+  end
 
-	def tocs(dst=$stdout)
-		@Url2Page.each do |href,p|
-			$stdout.puts href
-			p.each_h do |level,txt|
-				t = "#{txt}".strip
-				sep = "\t" * level
-				$stdout.puts "#{sep}<h#{level}>#{t}</#{level}>"
-			end
-		end
-	end
+  def tocs(dst=$stdout)
+    @Url2Page.each do |href,p|
+      $stdout.puts href
+      p.each_h do |level,txt|
+        t = "#{txt}".strip
+        sep = "\t" * level
+        $stdout.puts "#{sep}<h#{level}>#{t}</#{level}>"
+      end
+    end
+  end
 
-	def sitemap_dot(dst=$stdout,ignores=[],requires=[])
-		# build one regexp to match
-		tmp = []
-		ignores.each do |p|
-			p = Regexp.new(p) if !p.kind_of?(Regexp)
-			tmp << "(#{p.source})"
-		end
-		ignores = Regexp.new(tmp.join('|'))
+  def sitemap_dot(dst=$stdout,ignores=[],requires=[])
+    # build one regexp to match
+    tmp = []
+    ignores.each do |p|
+      p = Regexp.new(p) if !p.kind_of?(Regexp)
+      tmp << "(#{p.source})"
+    end
+    ignores = Regexp.new(tmp.join('|'))
 
-		dst.puts "#\# begin #{ignores.source}"
-		@Url2Page.each do |href,page|
-			page.each_a do |elem|
-				ref = elem.attributes['href']
-				next if ref.nil?
-				next if !ignores.match(ref).nil?
-				dst.puts "#{href} -> #{ref}"
-			end
-		end
-		dst.puts '# end'
-	end
+    dst.puts "#\# begin #{ignores.source}"
+    @Url2Page.each do |href,page|
+      page.each_a do |elem|
+        ref = elem.attributes['href']
+        next if ref.nil?
+        next if !ignores.match(ref).nil?
+        dst.puts "#{href} -> #{ref}"
+      end
+    end
+    dst.puts '# end'
+  end
 end
 
